7410b63c343789872f7950208258d559a532b4b1
teperyaemo <rokudenas@bk.ru>
2022-04-04 00:18:11 +0300

    Добавьте файлы проекта.

---
+++ preparation/AddEditPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace preparation
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Деталь _currentDetail = new Деталь();
+
+        public AddEditPage(Деталь selectedDetail)
+        {
+            if (selectedDetail != null) 
+            _currentDetail = selectedDetail;    
+            InitializeComponent();
+            CategoryCB.ItemsSource = rusmetEntities1.GetContext().Категория.ToList();
+            DataContext = _currentDetail;
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentDetail.Название))
+                errors.AppendLine("Введите Название");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;           
+            }
+
+            if (_currentDetail.Id_детали == 0)
+            {
+                rusmetEntities1.GetContext().Деталь.Add(_currentDetail);
+            }
+
+            try
+            {
+                rusmetEntities1.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
---
+++ preparation/detailPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace preparation
+{
+    /// <summary>
+    /// Логика взаимодействия для detailPage.xaml
+    /// </summary>
+    public partial class detailPage : Page
+    {
+        public detailPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var SelectedDetails = DetailDG.SelectedItems.Cast<Деталь>().ToList();
+
+            if(MessageBox.Show("Вы уверены?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question)==MessageBoxResult.Yes)
+            {
+                try
+                {
+                    rusmetEntities1.GetContext().Деталь.RemoveRange(SelectedDetails);
+                    rusmetEntities1.GetContext().SaveChanges();
+                    MessageBox.Show("Всё заебись");
+
+                    DetailDG.ItemsSource = rusmetEntities1.GetContext().Деталь.ToList();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                rusmetEntities1.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DetailDG.ItemsSource = rusmetEntities1.GetContext().Деталь.ToList();
+            }
+        }
+
+        private void EditBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Деталь));
+        }
+    }
+}
