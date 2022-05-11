fcaebcbddf6f97615737c5de20009bebcbbd7b46
teperyaemo <rokudenas@bk.ru>
2022-05-11 15:47:33 +0300

    111

---
+++ preparation/AdvancedAgentModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace preparation
+{
+    internal class AdvancedAgentModel
+    {
+        private readonly poprizhenokEntities poprizhenokEntities1;
+
+        public AdvancedAgentModel(poprizhenokEntities poprizhenokEntities)
+        {
+            this.poprizhenokEntities1 = poprizhenokEntities;
+        }
+
+        public Agents agents(int id) => poprizhenokEntities.GetContext().Agents.FirstOrDefault(p => p.ИН_Агента == id);
+        public int CountOfSales(int id) => Convert.ToInt32(poprizhenokEntities.GetContext().productsale.Where(p => p.ИН_Агента == id && p.Дата.Value.Year >= DateTime.Today.Year).Sum(p => p.Кол_во));
+        public int AgentSale(int id)
+        {
+            int Count = Convert.ToInt32(poprizhenokEntities.GetContext().productsale.Where(p => p.ИН_Агента == id).Sum(p => p.Кол_во));
+            if (Count >= 500) return 25;
+            else if (Count >= 150) return 20;
+            else if (Count >= 50) return 10;
+            else if (Count >= 10) return 5;
+            else return 0;
+        }
+    }
+}
--- preparation/AgentPage.xaml.cs
+++ preparation/AgentPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,7 +31,7 @@ namespace preparation
             Manager.MainFrame.Navigate(new AddEditPage(null));
         }
 
-        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        /*private void DeleteBtn_Click(object sender, RoutedEventArgs e)
         {
             var SelectedDetails = DetailDG.SelectedItems.Cast<Agents>().ToList();
 
@@ -49,14 +50,14 @@ namespace preparation
                     MessageBox.Show(ex.Message.ToString());
                 }
             }
-        }
+        }*/
 
         private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             if (Visibility == Visibility.Visible)
             {
                 poprizhenokEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
-                DetailDG.ItemsSource = poprizhenokEntities.GetContext().Agents.ToList();
+                LViewAgents.ItemsSource = poprizhenokEntities.GetContext().Agents.ToList();
             }
         }
 
@@ -64,5 +65,30 @@ namespace preparation
         {
             Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Agents));
         }
+/*
+        private DataTable ReturnDataView()
+        {
+            DataTable dataTable = new DataTable("Agents");
+            dataTable.Columns.Add("ИН", typeof(int));
+            dataTable.Columns.Add("Наименование", typeof(string));
+            dataTable.Columns.Add("Кол-во продаж за год", typeof(int), "CountOfSales(ИН)");
+            dataTable.Columns.Add("Размер скидки", typeof(int), "AgentSale(ИН)");
+            dataTable.Columns.Add("Телефон", typeof(string));
+            dataTable.Columns.Add("Тип агента", typeof(string));
+
+
+            return dataTable;
+        }
+
+        public int CountOfSales(int id) => Convert.ToInt32(poprizhenokEntities.GetContext().productsale.Where(p => p.ИН_Агента == id && p.Дата.Value.Year >= DateTime.Today.Year).Sum(p => p.Кол_во));
+        public int AgentSale(int id)
+        {
+            int Count = Convert.ToInt32(poprizhenokEntities.GetContext().productsale.Where(p => p.ИН_Агента == id).Sum(p => p.Кол_во));
+            if (Count >= 500) return 25;
+            else if (Count >= 150) return 20;
+            else if (Count >= 50) return 10;
+            else if (Count >= 10) return 5;
+            else return 0;
+        }*/
     }
 }
